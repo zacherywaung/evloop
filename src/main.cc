9d46e4983b37151ee2325393f780b018c4dc4ba5
zacherywaung <2691205573@qq.com>
2026-09-05 12:00:50 -0700

    feat(buffer): test Read/Write operations

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,18 @@
 #include "server.hpp"
+#include <iostream>
 
 int main()
 {
     Buffer buf;
+    for(int i = 0; i <= 300; i++)
+    {
+        std::string tmp = "hello!" + std::to_string(i) + "\n";
+        buf.WriteAndMoveString(tmp);
+    }
+    // while(buf.ReadableSize() > 0)
+    // {
+    //     std::cout << buf.GetLineAndMove() << std::endl;
+    // }
+    std::string out = buf.ReadAsStringAndMove(buf.ReadableSize());
+    std::cout << out << std::endl;
 }
