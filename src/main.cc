fb51aba16b576fd8ffe8cfeed48bc278915e5c71
zacherywaung <2691205573@qq.com>
2026-09-05 12:32:00 -0700

    feat(log): implement logging macros with info/debug/error level

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,23 @@
 #include "server.hpp"
 #include <iostream>
 
+#define INFO 0
+#define DEBUG 1
+#define ERR 2
+#define DEFAULT_LOG_LEVEL DEBUG
+
+#define LOG(level, format, ...) do{\
+    if(level < DEFAULT_LOG_LEVEL) break;\
+    time_t t = time(nullptr);\
+    struct tm* ltm = localtime(&t);\
+    char tmp[32] = {0};\
+    strftime(tmp, 31, "%H:%M:%S", ltm);\
+    fprintf(stdout, "%s %s:%d " format "\n", tmp, __FILE__, __LINE__, ##__VA_ARGS__);\
+}while(0)
+#define INFO_LOG(format, ...) LOG(INFO, format, ##__VA_ARGS__)
+#define DEBUG_LOG(format, ...) LOG(DEBUG, format, ##__VA_ARGS__)
+#define ERR_LOG(format, ...) LOG(ERR, format, ##__VA_ARGS__)
+
 int main()
 {
     Buffer buf;
@@ -13,6 +30,11 @@ int main()
     // {
     //     std::cout << buf.GetLineAndMove() << std::endl;
     // }
-    std::string out = buf.ReadAsStringAndMove(buf.ReadableSize());
-    std::cout << out << std::endl;
+    // std::string out = buf.ReadAsStringAndMove(buf.ReadableSize());
+    // std::cout << out << std::endl;
+    for(int i = 0; i < 5; i++)
+    {
+        INFO_LOG("hello");
+        ERR_LOG("hello");
+    }
 }
